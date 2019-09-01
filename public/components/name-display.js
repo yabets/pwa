6663a550123580ab4a37bed397d74f98bc2b9d3c
yabets <yabetsbelay@gmail.com>
2019-09-01 18:52:08 +0530

    fix font awesome icon to show gender and fav icon

--- public/components/name-display.js
+++ public/components/name-display.js
@@ -1,10 +1,10 @@
 let nameDisplay = document.createElement('template');
     nameDisplay.innerHTML = `
+        <link rel="stylesheet" href="lib/font-awesome-4.7.0/css/font-awesome.min.css">
         <style>
             @import"style.css"
         </style> 
-            <div class="box">
-            </div>
+        <div class="box"></div>
         <slot></slot> <!-- slotted content appears here -->
         `;
 let notFound = document.createElement('template');
@@ -13,30 +13,32 @@ const getFormatedView = ({name, gender, meaning}) => {
     // TODO::check if the name is in favorite
 
     // TODO:: Add favorite icon
-
+    const showGenderIcon = (gender) => {
+        if(gender === 'unisex'){
+            return '<i class="fa fa-male" aria-hidden="true"></i><i class="fa fa-female" aria-hidden="true"></i>'
+        } else {
+            return `<i class="fa fa-${gender}" aria-hidden="true"></i>`
+        }
+    }
     return `<div class="col-70 left">
-    <div class="row">
-        <span class="label">Name</span>
-        <span class="value">${name}</span>
-    </div>
-    <div class="row">
-        <span class="label">Gender</span>
-        <span class="value">${gender}</span>
-    </div>
-    </div>
-    <div class="col-30 right">
-    <span class="icon">
-        <i class="fa fa-${gender}" aria-hidden="true"></i>
-        male
-    </span>
-    </div>
+                <div class="row">
+                    <span class="label">Name</span>
+                    <span class="value">${name}</span>
+                </div>
+                <div class="row">
+                    <span class="label">Gender</span>
+                    <span class="value">${gender}</span>
+                </div>
+            </div>
+            <div class="col-30 right">
+                <div class="fav">
+                    <span><i class="fa fa-star"></i></span>
+                </div>
+                <span class="icon">${showGenderIcon(gender)}</span>
+            </div>
     <!-- TODO:: import font awesome fav icon style on hover to indicate clickable -->
     <!-- TODO:: import font awesome and show approprate geneder icon -->
-    <div class="fav">
-    <span>
-        <i class="far fa-star"></i>
-    </span>
-    </div>`;
+    `;
 }
 class NameDisplay extends HTMLElement {
     // A getter/setter for a search property.
