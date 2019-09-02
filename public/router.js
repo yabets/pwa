bf000a9cfa23e5c19991f6b9de5b2cae2ac1683a
yabets <yabetsbelay@gmail.com>
2019-09-02 13:36:06 +0530

    fix route for reload

--- public/router.js
+++ public/router.js
@@ -15,4 +15,16 @@
         }
         nav.setAttribute('active', location.hash);
    }
+   window.onload = () => {
+        if(location.hash === "#home") {
+            main.innerHTML = "<home-search></home-search>";
+        } else if(location.hash === "#favorites") {
+            main.innerHTML = "<fav-orite></fav-orite>";
+        } else if(location.hash === "#login") {
+            main.innerHTML = "<log-in></log-in>";
+        } else {
+            main.innerHTML = "<name-display></name-display>"
+        }
+        nav.setAttribute('active', location.hash);
+   }
 })()
